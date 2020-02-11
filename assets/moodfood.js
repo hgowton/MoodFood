552ca85b11e1e012933c95ce03c7cf5d11aa858e
Cassandra Heil <Cassandraheil19@gmail.com>
2020-02-11 18:51:23 -0500

    added javascript

---
+++ assets/moodfood.js
@@ -0,0 +1,14 @@
+var image = $("<img>");
+
+image.addClass("user-image");
+
+image.attr("src", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.scribd.com%2Fupload-document&psig=AOvVaw303OYeHbnzwtu0FVR7lO9p&ust=1581459779872000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOiYhPqCyOcCFQAAAAAdAAAAABAD")
+
+
+// html 
+/* <form action="myform.cgi">
+<input type="file" name="fileupload" value="fileupload" id="fileupload">
+<label for="fileupload"> Select a file to upload</label>
+<br><input type="image" src="/wp-content/uploads/sendform.png" alt="Submit" width="100">
+</form> */
+
